c727fc745ca3651ce12e57163fee59cb01e731d0
Sri keerthivasan <srikeerthivasan111@gmail.com>
2025-05-06 12:44:39 +0530

    Small issue fixed

--- src/app/services/global.service.ts
+++ src/app/services/global.service.ts
@@ -8,8 +8,7 @@ import { ApiService } from './api.service';
 import { MessageComponent } from '../shared/message/message.component';
 import { Candidate } from '../models/candidates/candidate.model';
 import { LoginPopupComponent } from '../shared/popup/login-popup/login-popup.component';
-import { MobileMessageComponent } from '../shared/mobile-message/mobile-message.component';
- 
+
 @Injectable({
   providedIn: 'root',
 })
@@ -137,15 +136,6 @@ export class GlobalService {
     });
   }
 
-  mobileShowMessage(status: string, message: string) {
-    this.dialog.open(MobileMessageComponent, {
-    data: { message: message },
-    header: status,
-    width: '30%',
-    styleClass: 'responsive-dialog'
-  });
-}
-  
 
   parseTimeToString(time: any) {
     const date = new Date(time);
