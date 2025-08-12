eacf7740ff25ba49c39820e4ea5ea74b1e262e8c
Suman Raj <sumanravi1225@gmail.com>
2025-08-12 17:13:25 +0530

    changes in edit user

--- src/app/features/mobile-users-accounts/mob-view-account/mob-view-account.component.ts
+++ src/app/features/mobile-users-accounts/mob-view-account/mob-view-account.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { ApiService } from 'src/app/services/api.service';
 import { GlobalService } from 'src/app/services/global.service';
